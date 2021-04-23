8a3fab67f9bb852e93a5d73cbd9e56ebdb9ae59c
Ritwik46 <ritwikjha46@gmailco>
2021-04-23 23:44:25 +0530

    few functions done

--- menu.py
+++ menu.py
@@ -1,25 +1,65 @@
 from tkinter import *
-#from functions import *
+import subprocess as sp
 
 root = Tk()
 root.title('Docker')
-root.geometry('500x600')
+root.geometry('500x650')
 
 #functions
 def imagelist():
-    return
+    output = sp.getoutput('docker images')
+    op_win = Toplevel()
+    op_win.title('Output')
+    Label(op_win, text=output, font=('arial',10)).pack()
 
 def running_conlist():
-    return
+    output = sp.getoutput('docker ps')
+    op_win = Toplevel()
+    op_win.title('Output')
+    Label(op_win, text=output, font=('arial',10)).pack()
 
 def all_conlist():
-    return
+    output = sp.getoutput('docker ps -a')
+    op_win = Toplevel()
+    op_win.title('Output')
+    Label(op_win, text=output, font=('arial',10)).pack()
+
+def image_pull_op():
+    output = sp.getoutput('docker pull {}'.format(image_name))
+    Label(image_pull_op_win, text=output).grid(row=2, column=0, columnspan=2)
 
 def image_pull():
-    return
+    global image_pull_op_win
+    image_pull_op_win = Toplevel()
+    image_pull_op_win.title('Output')
+    #image_pull_op_win.geometry('300x200')
+    e = Entry(image_pull_op_win)
+    Label(image_pull_op_win, text='Enter name of image').grid(row=0, column=0, padx=5, pady=10)
+    e.grid(row=0, column=1, pady=10, padx=5)
+    global image_name
+    image_name = e.get()
+    Button(image_pull_op_win, text='Submit', command=image_pull_op).grid(row=1, column=0, columnspan=2, padx=10)
+
+def docker_run_op():
+    output = sp.getoutput('docker run -it --name {} {}'.format(con_name, docker_run_image))
+    Label(docker_run_op_win, text=output).grid(row=3, column=0, columnspan=2, padx=10)
 
 def docker_run():
-    return
+    global docker_run_op_win
+    docker_run_op_win = Toplevel()
+    docker_run_op_win.title('Output')
+    #image_pull_op_win.geometry('300x200')
+    e1 = Entry(docker_run_op_win)
+    Label(docker_run_op_win, text='Enter name of container: ').grid(row=0, column=0, padx=5, pady=10)
+    e1.grid(row=0, column=1, pady=10, padx=5)
+    global con_name
+    con_name = e1.get()
+    e2 = Entry(docker_run_op_win)
+    Label(docker_run_op_win, text='Enter name of image: ').grid(row=1, column=0, padx=5, pady=10)
+    e2.grid(row=1, column=1, pady=10, padx=5)
+    global docker_run_image
+    docker_run_image = e2.get()
+    Button(docker_run_op_win, text='Submit', command=docker_run_op).grid(row=2, column=0, columnspan=2, padx=10)
 
 def docker_run_bg():
     return
@@ -103,21 +143,21 @@ Radiobutton(root, text='List the docker images', variable=value, value=1).grid(r
 Radiobutton(root, text='List the running docker containers', variable=value, value=2).grid(row=5, column=0, sticky='W', padx=x)
 Radiobutton(root, text='List all the docker containers', variable=value, value=3).grid(row=6, column=0, sticky='W', padx=x)
 Radiobutton(root, text='Pull a docker image', variable=value, value=4).grid(row=7, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Run a docker container with interactive terminal', variable=value, value=5).grid(row=7, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Run a docker container in background', variable=value, value=6).grid(row=8, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Foreground a container running in background', variable=value, value=7).grid(row=9, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Stop a running container', variable=value, value=8).grid(row=10, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Terminate a stopped container', variable=value, value=9).grid(row=11, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Stop all the containers', variable=value, value=10).grid(row=12, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Terminate all the stopped containers', variable=value, value=11).grid(row=13, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Delete a docker image', variable=value, value=12).grid(row=14, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Run a command in docker container', variable=value, value=13).grid(row=15, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Run a command in docker container and then termiate it', variable=value, value=14).grid(row=16, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Inspect a running container', variable=value, value=15).grid(row=17, column=0, sticky='W', padx=x)
-Radiobutton(root, text='Get logs of running container', variable=value, value=16).grid(row=18, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Run a docker container with interactive terminal', variable=value, value=5).grid(row=8, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Run a docker container in background', variable=value, value=6).grid(row=9, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Foreground a container running in background', variable=value, value=7).grid(row=10, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Stop a running container', variable=value, value=8).grid(row=11, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Terminate a stopped container', variable=value, value=9).grid(row=12, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Stop all the containers', variable=value, value=10).grid(row=13, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Terminate all the stopped containers', variable=value, value=11).grid(row=14, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Delete a docker image', variable=value, value=12).grid(row=15, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Run a command in docker container', variable=value, value=13).grid(row=16, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Run a command in docker container and then termiate it', variable=value, value=14).grid(row=17, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Inspect a running container', variable=value, value=15).grid(row=18, column=0, sticky='W', padx=x)
+Radiobutton(root, text='Get logs of running container', variable=value, value=16).grid(row=19, column=0, sticky='W', padx=x)
 
 
-Button(root, text='Submit', command=getvalue, padx=20, pady=15).grid(row=19, column=0, pady=30)
+Button(root, text='Submit', command=getvalue, padx=20, pady=15).grid(row=20, column=0, pady=30)
 
 
 root.mainloop()
